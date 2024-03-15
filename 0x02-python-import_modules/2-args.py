298122de308a7191fe071b2825ce6cb60861fdd2
Mahmoud Ismail <ismailm7373@gmail.com>
2024-03-15 01:25:49 +0000

    task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import sys
-n = len(sys.argv) - 1
-print(n, end=" ")
-if n == 0:
-    print("arguments.")
-elif n == 1:
-    print("argument:")
-else:
-    print("arguments:")
-if n > 0:
-    for i in range(1, n+1):
-        print("{:d}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__"
+    n = len(sys.argv) - 1
+    print(n, end=" ")
+    if n == 0:
+        print("arguments.")
+    elif n == 1:
+        print("argument:")
+    else:
+        print("arguments:")
+    if n > 0:
+        for i in range(1, n+1):
+            print("{:d}: {}".format(i, sys.argv[i]))
