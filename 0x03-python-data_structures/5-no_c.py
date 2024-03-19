e7c4fe04c4c14bcf911590fcb03f3d18121d1ce8
Mahmoud Ismail <ismailm7373@gmail.com>
2024-03-19 13:23:31 +0000

    task

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,3 @@
 #!/usr/bin/pyhton3
 
 def no_c(my_string):
-    n = -1
-    new_string = my_string
-    for c in new_string:
-        n += 1
-        if c == 'c' or c == 'C':
-            new_string[n:n + 1] = []
-    print(new_string)
