857c8aeb2f30a27b008c652fd8b12f4ac54ef9e0
Mahmoud Ismail <ismailm7373@gmail.com>
2024-03-19 12:58:34 +0000

    task

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,10 @@
 #!/usr/bin/pyhton3
 
 def no_c(my_string):
-    new_string = my_string[:]
+    n = -1
+    new_string = my_string
     for c in new_string:
+        n += 1
         if c == 'c' or c == 'C':
-            n = index(c)
             new_string[n:n + 1] = []
     print(new_string)
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    l = len(matrix)
-    for i in range(l):
-        ll = len(matrix[i])
-        for j in range(ll):
-            if j < ll - 1:
+    for i in matrix:
+        for j in i:
+            if j < len(i) - 1:
                 print("{:d}".format(matrix[i][j]), end=' ')
             else:
                 print("{:d}".format(matrix[i][j]))
