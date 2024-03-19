4ae8dd1226ed98890ab8332068ba33fd619a68c5
Mahmoud Ismail <ismailm7373@gmail.com>
2024-03-19 00:44:06 +0000

    task

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,9 @@
+#!/bin/usr/python3
+
+def replace_in_list(my_list, idx, element):
+    l = len(my_list)
+    if idx < 0 or idx >= l:
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
