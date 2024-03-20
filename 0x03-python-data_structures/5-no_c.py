ce297067a8b118854063febe0db1ef26141167db
Mahmoud Ismail <ismailm7373@gmail.com>
2024-03-20 05:45:27 +0000

    task

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,3 +1,13 @@
 #!/usr/bin/pyhton3
 
 def no_c(my_string):
+    ntext = ''
+    nlist = []
+    nlist += my_string
+    for i in 'Cc':
+        n = nlist.count(i)
+        for j in range(n):
+            nlist.remove(i)
+    for i in nlist:
+        ntext += i
+    return ntext
