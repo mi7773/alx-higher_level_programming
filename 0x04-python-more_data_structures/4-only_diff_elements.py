4988e8ff32125ee0155393c1215d9f5faf1f331a
Mahmoud Ismail <ismailm7373@gmail.com>
2024-03-21 01:05:19 +0000

    task

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,24 +1,12 @@
 #!/usr/bin/python3
 
 def only_diff_elements(set_1, set_2):
-    od_set_1 = set()
-    od_set_2 = set()
-    od_set = set()
-    n1 = len(set_1)
-    n2 = len(set_2)
-    if n1 > n2:
-        od_Set_1 = set_1.copy()
-        od_set = set_1.copy()
-        od_set_2 = set_2.copy()
-    else:
-        od_set_1 = set_2.copy()
-        od_set = set_2.copy()
-        od_set_2 = set_1.copy()
-    for i in od_set_1:
-        for j in od_set_2:
+    temp = set_1.copy()
+    od_set = set_1.copy()
+    for i in set_2:
+        for j in temp:
             if i == j:
                 od_set.remove(i)
                 break
-            else:
-                od_set.add(j)
+            od_set.add(i)
     return od_set
