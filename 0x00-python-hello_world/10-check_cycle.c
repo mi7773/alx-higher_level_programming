64adc571521cfb22b0f33675ca3b4455c5b3f0be
Mahmoud Ismail <ismailm7373@gmail.com>
2024-03-23 20:05:11 +0000

    task

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * check_cycle - draft
+ * @list: draft
+ *
+ * Return: draft
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *t;
+	listint_t *c;
+	int r = 0;
+
+	while(list && r == 0)
+	{
+		t = list;
+		c = list;
+		while(t)
+		{
+			if (c == t->next)
+			{
+				r = 1;
+				break;
+			}
+			t = t->next;
+		}
+		list = list->next;
+	}
+	return (r);
+}
