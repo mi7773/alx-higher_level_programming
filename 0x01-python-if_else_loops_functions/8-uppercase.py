23196db9048a3fd7f61e4d20cb889f82da2e4070
Mahmoud Ismail <ismailm7373@gmail.com>
2024-04-11 18:20:58 +0200

    task

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     """prints a string in uppercase followed by a new line."""
-    # str[0] = 'i' strings in Python are immutable.
+    # str[0] = 'i' we can't since strings in Python are immutable.
     new_string = ""
     for c in str:
         i = ord(c)
