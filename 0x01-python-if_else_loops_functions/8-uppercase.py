f832871bb69d126dfeb0663f4686938984f11fa4
Mahmoud Ismail <ismailm7373@gmail.com>
2024-04-11 18:20:01 +0200

    task

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def uppercase(str):
     """prints a string in uppercase followed by a new line."""
+    # str[0] = 'i' strings in Python are immutable.
+    new_string = ""
     for c in str:
         i = ord(c)
         if i <= 122 and i >= 97:
             i -= 32
-        print("{:c}".format(i), end="")
-    print("{:c}".format(10), end="")
+        new_string += chr(i)
+    print("{:s}".format(new_string))
