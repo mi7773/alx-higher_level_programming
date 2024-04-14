526facab52c70beff9542c15b030907e6271c7ec
Mahmoud Ismail <ismailm7373@gmail.com>
2024-04-14 15:43:49 +0200

    task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,9 @@
 if __name__ == "__main__":
     from sys import argv
     list_p = ['arguments.', 'argument:', 'arguments:']
-    print('{:d} {:s}'.format(len(argv) - 1, list_p[1] if len(argv) == 2 else list_p[0] if len(argv) == 1 else list_p[2]))
+    print('{:d} {:s}'.format(len(argv) - 1, list_p[1]
+                             if len(argv) == 2 else list_p[0]
+                             if len(argv) == 1 else list_p[2]))
     for i in argv:
         if i == argv[0]:
             continue
