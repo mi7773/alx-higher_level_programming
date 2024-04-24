b9477e7baddadadb2a409a68be6d15a6ea478751
Mahmoud Ismail <ismailm7373@gmail.com>
2024-04-24 20:22:58 +0200

    task

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,9 +2,8 @@
 
 def no_c(my_string):
     """removes all characters c and C from a string."""
-    new_string = list(my_string)
     n_string = ''
-    for i in new_string:
+    for i in my_string:
         if i != 'c' and i != 'C':
             n_string += '{:s}'.format(i)
     return n_string
