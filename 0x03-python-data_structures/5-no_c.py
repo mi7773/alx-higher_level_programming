54dbb4e352865da69538ca384e0b6959cd663931
Mahmoud Ismail <ismailm7373@gmail.com>
2024-04-24 22:00:40 +0200

    task

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,5 +5,5 @@ def no_c(my_string):
     n_string = ''
     for i in my_string:
         if i != 'c' and i != 'C':
-            n_string += '{:s}'.format(i)
+            n_string += i
     return n_string
