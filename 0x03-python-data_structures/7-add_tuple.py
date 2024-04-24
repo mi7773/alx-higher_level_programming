d6f9d51f0342c8c18dcf6d9d609091c4a2f2afd3
Mahmoud Ismail <ismailm7373@gmail.com>
2024-04-25 01:38:41 +0200

    task

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,3 +2,9 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
     """adds 2 tuples."""
+    result = [0, 0]
+    for i in range(2):
+        for t in [tuple_a, tuple_b]:
+            if len(t) >= i + 1 and t is not None:
+                result[i] += t[i]
+    return tuple(result)
