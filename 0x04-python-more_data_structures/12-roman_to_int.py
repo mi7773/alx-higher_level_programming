a7ce1c14c5d2858e65229b2c4dbfabf09f8fe2ac
Mahmoud Ismail <ismailm7373@gmail.com>
2024-05-05 21:09:08 +0300

    task

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    """converts a Roman numeral to an integer."""
+    result = 0
+    v = 0
+    c = 0
+    roman_dict = {'I': 1, 'V': 5, 'X': 10,
+                  'L': 50, 'C': 100, 'D': 500,
+                  'M': 1000}
+    if roman_string is None or roman_string.isalpha() is False:
+        return 0
+    for i in roman_string:
+        if roman_dict.get(i) is None:
+            return 0
+        result += roman_dict[i]
+        if roman_dict[i] > v and c != 0:
+            result -= 2 * v
+        v = roman_dict[i]
+        c += 1
+    return result
