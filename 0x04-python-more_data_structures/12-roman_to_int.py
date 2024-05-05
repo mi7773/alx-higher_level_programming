1a0abe45a731c86f31c4c536b54623cd8f7fffad
Mahmoud Ismail <ismailm7373@gmail.com>
2024-05-05 21:10:29 +0300

    task

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,7 +7,7 @@ def roman_to_int(roman_string):
     roman_dict = {'I': 1, 'V': 5, 'X': 10,
                   'L': 50, 'C': 100, 'D': 500,
                   'M': 1000}
-    if roman_string is None or roman_string.isalpha() is False:
+    if roman_string is None:
         return 0
     for i in roman_string:
         if roman_dict.get(i) is None:
