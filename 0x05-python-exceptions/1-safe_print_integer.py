852137f07aa1df81ca526a91a7467459aeabf510
Mahmoud Ismail <ismailm7373@gmail.com>
2024-05-09 07:35:44 +0300

    task

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,8 +4,7 @@ def safe_print_integer(value):
     """prints an integer with "{:d}".format()"""
     try:
         r = True
-        result = value + 0
-        print(value)
-    except TypeError:
+        print(F"{value:d}")
+    except ValueError:
         r = False
     return r
