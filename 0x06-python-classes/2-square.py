5ddeb23e0f5da8a8e1b0cd2291cd4bc98a31d770
Mahmoud Ismail <ismailm7373@gmail.com>
2024-05-27 02:27:23 +0300

    task

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+Contains a Square class.
+"""
+
+
+class Square(object):
+    """
+    An empty class that defines a square.
+    """
+    def __init__(self, size=0):
+        """
+        Initializes objects.
+
+        Args:
+            size: Size of the square,
+                it must be integer and >= 0,
+                equals 0 if it is not given.
+        """
+        if type(size) is str:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
