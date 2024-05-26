adcd2ea89acb5ab36e5abadf5ff32856f98050d4
Mahmoud Ismail <ismailm7373@gmail.com>
2024-05-27 02:07:22 +0300

    task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+Contains a Square class
+"""
+
+
+class Square(object):
+    """
+    an empty class that defines a square.
+
+    the __init__ method initializes objects
+
+    Args:
+        size : size of the square
+    """
+    def __init__(self, size):
+        self.__size = size
