7f4eae6a48b583a9f6fc9c95cc29534a17e48dd1
Mahmoud Ismail <ismailm7373@gmail.com>
2024-06-11 20:38:04 +0300

    task

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -8,6 +8,10 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_max_integer(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([9, 2, 3, 4]), 9)
+        self.assertEqual(max_integer([1, 2, 8, 4]), 8)
+        self.assertEqual(max_integer([1, -2, 3, 4]), 4)
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
         self.assertEqual(max_integer([]), None)
         self.assertEqual(max_integer([4]), 4)
         self.assertEqual(max_integer([1.5, 1.3, 1.2, 1.1]), 1.5)
