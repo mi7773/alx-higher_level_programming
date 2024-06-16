76c2262832b7097482a4d9929d6b8cc819a0cb11
Mahmoud Ismail <ismailm7373@gmail.com>
2024-06-16 10:44:53 +0300

    task

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/pyhton3
+
+"""
+0-lookup module.
+"""
+
+
+def lookup(obj):
+    """
+    Returns the list of available attributes
+        and methods of an object.
+
+    Args:
+        obj (object): The object.
+
+    Returns:
+        list: A list of attributes and methods.
+    """
+    return dir(object)
