dbacdc85b750eb15ce73393a2201256193aad8cb
Mahmoud Ismail <ismailm7373@gmail.com>
2024-06-19 23:24:52 +0300

    task

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -16,6 +16,8 @@ def save_to_json_file(my_obj, filename):
         my_obj (any tybe): The object.
         filename: The file name.
     """
-    write_file = __import__('1-write_file').write_file
     text = json.dumps(my_obj)
-    write_file(filename, text)
+    if not text:
+        text = ""
+    with open(filename, mode='w', encoding='utf-8') as file:
+        file.write(text)
