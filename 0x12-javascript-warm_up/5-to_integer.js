988f8853b2a87481ce418dfb3dc28b5bbe9806a0
Mahmoud Ismail <ismailm7373@gmail.com>
2024-07-15 10:49:09 +0300

    task

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,7 +5,7 @@ const { argv } = require('node:process');
 const val = argv[2];
 
 if (Number(val) === Number(argv[2])) {
-  console.log(Number(val));
+  console.log('My number:', Number(val));
 } else {
   console.log('Not a number');
 }
