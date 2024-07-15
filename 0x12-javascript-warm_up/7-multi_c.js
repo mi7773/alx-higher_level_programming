114d0f509618039d58a481bf21f3df5f63a271aa
Mahmoud Ismail <ismailm7373@gmail.com>
2024-07-15 10:47:21 +0300

    task

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+
+const val = Number(argv[2]);
+
+if (val === Number(argv[2])) {
+  for (i = 0; i < val; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
