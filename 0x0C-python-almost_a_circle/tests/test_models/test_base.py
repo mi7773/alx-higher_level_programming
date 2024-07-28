692d23c29df4723e77ce375547a40fad241952ce
Mahmoud Ismail <ismailm7373@gmail.com>
2024-07-28 14:10:48 +0300

    task

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -7,14 +7,14 @@ from models.base import Base
 class TestBase(unittest.TestCase):
     """ TestBase class """
 
-    def test_id_automatic_assignment(self):
-        """ Testing id """
+    def test_id_auto_assign(self):
+        """ Testing id automatic assignment """
         self.assertEqual(Base().id, 1)
 
     def test_id_increment(self):
         """ Testing id increment """
         self.assertEqual(Base().id, 2)
 
-    def test_id_manual_assignment(self):
-        """ Testing id """
+    def test_id_manual_assign(self):
+        """ Testing id manual assignment """
         self.assertEqual(Base(99).id, 99)
