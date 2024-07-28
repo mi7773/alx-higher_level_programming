9ec0125662a6b8e217a31ae31013af56b81101f4
Mahmoud Ismail <ismailm7373@gmail.com>
2024-07-28 12:42:35 +0300

    task

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -3,5 +3,13 @@ from models.base import Base
 """ test_base """
 
 
-class TestBaseClass(unittest.TestCase):
-    """ 
+class TestBase(unittest.TestCase):
+    """ TestBaseClass class """
+
+    def test_init(self):
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+        b3 = Base()
+        self.assertEqual(b3.id, 3)
