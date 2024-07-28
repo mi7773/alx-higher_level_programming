975f64db7f124bce389fe279b269c297779eb153
Mahmoud Ismail <ismailm7373@gmail.com>
2024-07-28 14:38:08 +0300

    task

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -7,12 +7,12 @@ from models.rectangle import Rectangle
 class TestRectangle(unittest.TestCase):
     """ TestRectangle class """
 
-    def test_x_optional(self):
+    def test_x_opt_val(self):
         """ Testing x optional value """
 
         self.assertEqual(Rectangle(1,2).x, 0)
 
-    def test_y_optional(self):
+    def test_y_opt_val(self):
         """ Testing y optional value """
 
         self.assertEqual(Rectangle(1,2).y, 0)
