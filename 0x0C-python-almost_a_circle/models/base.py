245b0d42743b0d11e5a8ee9d549bb16983a947ab
Mahmoud Ismail <ismailm7373@gmail.com>
2024-07-28 12:14:11 +0300

    task

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" base """
+
+
+class Base(object):
+    """ Base class """
+
+    __nb_objects = 0
+    """ int: The number of objects class attribute """
+
+    def __init__(self, id=None):
+        """
+        Initializes instances.
+
+        Args:
+            id (int): The id.
+        """
+
+        Base.__nb_objects += 1
+
+        if id is None:
+            self.id = Base.__nb_objects
+            """ int: The id instance attribute """
+        else:
+            self.id = id
