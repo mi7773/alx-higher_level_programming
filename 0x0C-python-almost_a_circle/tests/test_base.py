1f0b0cedba7d5c2bc23e1c94d962984e47219312
Mahmoud Ismail <ismailm7373@gmail.com>
2024-07-28 13:29:35 +0300

    task

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -12,5 +12,5 @@ class TestBase(unittest.TestCase):
         self.assertEqual(b1.id, 1)
         b2 = Base()
         self.assertEqual(b2.id, 2)
-        b3 = Base()
-        self.assertEqual(b3.id, 3)
+        b3 = Base(15)
+        self.assertEqual(b3.id, 15)
