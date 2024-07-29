08e91e2b20d5ff212a8b777382fb14523eb1af00
Mahmoud Ismail <ismailm7373@gmail.com>
2024-07-29 06:23:24 +0300

    task

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -11,6 +11,7 @@ class TestRectangle(unittest.TestCase):
         self.r1 = Rectangle(1, 2)
         self.r2 = Rectangle(1, 2, 3)
         self.r3 = Rectangle(1, 2, 3, 4)
+        self.r4 = Rectangle(1, 2, 3, 4, 5)
 
     def tearDown(self):
         """ Cleaning up the test environment """
@@ -43,12 +44,23 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(self.r3.height, 2)
         self.assertEqual(self.r3.id, 3)
 
+    def test_no_id(self):
+        """ Testing not given id value """
+        self.assertIsNotNone(self.r4)
+        self.assertEqual(self.r4.x, 3)
+        self.assertEqual(self.r4.y, 4)
+        self.assertEqual(self.r4.width, 1)
+        self.assertEqual(self.r4.height, 2)
+        self.assertEqual(self.r4.id, 5)
+
     def test_width_val(self):
         """ Testing the width validation """
         with self.assertRaises(TypeError):
             self.r1.width = '1'
         with self.assertRaises(ValueError):
             self.r1.width = -1
+        with self.assertRaises(ValueError):
+            self.r1.width = 0
 
     def test_height_val(self):
         """ Testing the height validation """
@@ -56,6 +68,8 @@ class TestRectangle(unittest.TestCase):
             self.r1.height = '1'
         with self.assertRaises(ValueError):
             self.r1.height = -1
+        with self.assertRaises(ValueError):
+            self.r1.height = 0
 
     def test_x_val(self):
         """ Testing the x validation """
