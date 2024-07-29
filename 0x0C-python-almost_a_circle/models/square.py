d2d4004719997691c3384f0a3c8b0829bbd7f136
Mahmoud Ismail <ismailm7373@gmail.com>
2024-07-29 15:59:43 +0300

    task

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -17,6 +17,10 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
+    def __str__(self):
+        """ Draft """
+        return f'[Square] ({self.id}) {self.x}/{self.y} - {self.size}'
+
     @property
     def size(self):
         return super().width
