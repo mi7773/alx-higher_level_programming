0d39fa1f051d7a38d89e4ad05da79ac85671a4fc
Mahmoud Ismail <ismailm7373@gmail.com>
2024-07-30 12:29:34 +0300

    task

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ base """
+import json
 
 
 class Base(object):
@@ -25,3 +26,18 @@ class Base(object):
     def reset(cls):
         """ Resets nb_objects class attribute """
         cls.__nb_objects = 0
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Returns the JSON string representation of list_dictionaries.
+
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+
+        Returns:
+            str: JSON representation.
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return '[]'
+        return json.dumps(list_dictionaries)
